96441e9f1c2309a0a9da4f135dc89aafe5ce2015
unknown <motikonisimon@gmail.com>
2025-12-12 15:44:08 +0200

    profile screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import LoginScreen from "./src/screens/Auth/LoginScreen";
 import RegisterScreen from "./src/screens/Auth/RegisterScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -12,6 +13,7 @@ export default function App() {
             <Stack.Navigator screenOptions = {{ headerShown: false }}>
                 <Stack.Screen name = "Login" component = {LoginScreen} />
                 <Stack.Screen name = "Register" component = {RegisterScreen} />
+                <Stack.Screen name = "Profile" component = {ProfileScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
